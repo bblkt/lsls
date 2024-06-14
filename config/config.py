a664ced705affdc639024d3c72d8cb572e7e62a8
kiokaq <169169161+kiokaq@users.noreply.github.com>
2024-06-14 22:08:57 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Newwissam/Mira",
+    "https://github.com/kiokaq/kdkd",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
