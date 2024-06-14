9f1d130d6440fc09d81a0191d3f0a60a54a47819
kiokaq <169169161+kiokaq@users.noreply.github.com>
2024-06-14 22:09:57 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kiokaq/kdkd",
+    "https://github.com/Newwissam/lsls",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
